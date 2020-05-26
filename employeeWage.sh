2c395e7f2d4c6f25d82ecb6f86b7c54c935751f8
“saad297” <skamil60@gmail.com>
2020-05-26 13:14:48 +0530

    Added : MonthlyCalculation

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,6 +2,7 @@
 echo "Welcome to the employee wage program."
 
 WAGE_PER_HOUR=20
+WORKING_DAYS_PER_MONTH=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
 PART_TIME=2
@@ -11,11 +12,13 @@ randomCheck=$((RANDOM%3))
 case $randomCheck in
 	$FULL_TIME)
 		DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	        echo "Daily wage of the employee for full time is : $DAILY_WAGE"
+		MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
+	        echo "Monthly wage of the employee for full time is : $MONTHLY_WAGE"
 		;;
 	$PART_TIME)
 		DAILY_WAGE=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
-	        echo "Daily wage of the employee for part time is : $DAILY_WAGE"
+		 MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
+	        echo "Monthly wage of the employee for part time is : $MONTHLY_WAGE"
 		;;
 	*)
 	echo "Employee is absent"
