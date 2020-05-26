f8eafe42215785dd3ed1c912c3ab75d89c192ba7
“saad297” <skamil60@gmail.com>
2020-05-26 13:02:13 +0530

    Added : Switch Case

--- employeeWage.sh
+++ employeeWage.sh
@@ -7,14 +7,19 @@ PART_TIME_HOUR=4
 PART_TIME=2
 FULL_TIME=1
 randomCheck=$((RANDOM%3))
-if [ $randomCheck -eq $FULL_TIME ];
-then
-        DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	echo "Daily wage of the employee for full time is : $DAILY_WAGE"
-elif [ $randomCheck -eq $PART_TIME ]
-then
-	DAILY_WAGE=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
-        echo "Daily wage of the employee for part time is : $DAILY_WAGE"
-else
-        echo "Employee is absent"
-fi
+
+case $randomCheck in
+	$FULL_TIME)
+		DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
+	        echo "Daily wage of the employee for full time is : $DAILY_WAGE"
+		;;
+	$PART_TIME)
+		DAILY_WAGE=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
+	        echo "Daily wage of the employee for part time is : $DAILY_WAGE"
+		;;
+	*)
+	echo "Employee is absent"
+esac
+
+
+
