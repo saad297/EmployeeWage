b571c170389f2cc70413d91cf4060017b8bcfd2d
“saad297” <skamil60@gmail.com>
2020-05-26 12:33:30 +0530

    Added : Part Time Included

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,12 +3,18 @@ echo "Welcome to the employee wage program."
 
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
-isPresent=1
-randomCheck=$((RANDOM%2))
-if [ $isPresent -eq $randomCheck ];
+PART_TIME_HOUR=4
+PART_TIME=2
+FULL_TIME=1
+randomCheck=$((RANDOM%3))
+if [ $randomCheck -eq $FULL_TIME ];
 then
         DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-	echo "Daily wage of the employe is : $DAILY_WAGE"
+	echo "Daily wage of the employee for full time is : $DAILY_WAGE"
+elif [ $randomCheck -eq $PART_TIME ]
+then
+	DAILY_WAGE=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
+        echo "Daily wage of the employee for part time is : $DAILY_WAGE"
 else
         echo "Employee is absent"
 fi
