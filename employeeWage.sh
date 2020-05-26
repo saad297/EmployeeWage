2a137978e0581cd0f5c8754c331be69c633fb9fb
“saad297” <skamil60@gmail.com>
2020-05-26 12:11:36 +0530

    Added : Employee daily wage calculation

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,11 +1,14 @@
 
 echo "Welcome to the employee wage program."
 
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
 isPresent=1
-randomCheck=$((1 + RANDOM%2))
+randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ];
 then
-        echo "Employee is present"
+        DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
+	echo "Daily wage of the employe is : $DAILY_WAGE"
 else
         echo "Employee is absent"
 fi
