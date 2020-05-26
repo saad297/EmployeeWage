bb6c89a8360e6127b63f84b4e6715bfef0626039
“saad297” <skamil60@gmail.com>
2020-05-26 11:56:22 +0530

    Added : Attendance Check

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,2 +1,11 @@
 
 echo "Welcome to the employee wage program."
+
+isPresent=1
+randomCheck=$((1 + RANDOM%2))
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
