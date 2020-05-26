f24f04b9fc5eff548385efda9fe95e9e8b2e3fe6
“saad297” <skamil60@gmail.com>
2020-05-26 14:49:26 +0530

    Added : Condition For Working Hours

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,26 +3,32 @@ echo "Welcome to the employee wage program."
 
 WAGE_PER_HOUR=20
 WORKING_DAYS_PER_MONTH=20
+TOTAL_WORKING_HOURS=100
+PRESENT_HOURS=1
+DAY=1
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
 PART_TIME=2
 FULL_TIME=1
-randomCheck=$((RANDOM%3))
-
-case $randomCheck in
-	$FULL_TIME)
-		DAILY_WAGE=$(($WAGE_PER_HOUR * $FULL_DAY_HOUR))
-		MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
-	        echo "Monthly wage of the employee for full time is : $MONTHLY_WAGE"
-		;;
-	$PART_TIME)
-		DAILY_WAGE=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
-		 MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
-	        echo "Monthly wage of the employee for part time is : $MONTHLY_WAGE"
-		;;
-	*)
-	echo "Employee is absent"
-esac
 
 
+while [ $DAY -le $WORKING_DAYS_PER_MONTH ] && [ $PRESENT_HOURS -lt $TOTAL_WORKING_HOURS ]
+do
 
+	randomCheck=$((RANDOM%3))
+	case $randomCheck in
+		$FULL_TIME)
+			EMPHRS=8
+			;;
+		$PART_TIME)
+			EMPHRS=4
+			;;
+		*)
+		EMPHRS=0
+	esac
+	DAILY_WAGE=$(($WAGE_PER_HOUR * $EMPHRS))
+         MONTHLY_WAGE=$(($WORKING_DAYS_PER_MONTH * $DAILY_WAGE))
+         echo "Monthly wage of the employee is : $MONTHLY_WAGE"
+
+((DAY++))
+done
